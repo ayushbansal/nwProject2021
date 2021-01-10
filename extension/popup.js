17d4e531650a0a7f33e3df85f78f7d703e8bf5d9
joshah98 <josha.heimann@alumni.ubc.ca>
2021-01-10 08:18:50 -0800

    can now use add all button

--- extension/popup.js
+++ extension/popup.js
@@ -59,37 +59,3 @@ var logoutFunc = function(){
     console.log(response)
   })
 }
-
-var findIngredientsFunc = function findIngredients(obj, k) {
-  for (var key in obj) {
-    var value = obj[key]
-
-    if (k == key) {
-      return [k, value]
-    }
-
-    if (typeof(value) === "object" && !Array.isArray(value)) {
-      var y = findIngredients(value, k)
-      if (y && y[0] == k) return y
-    }
-    if (Array.isArray(value)) {
-      for (var i = 0; i < value.length; ++i) {
-        var x = findIngredients(value[i], k)
-        if (x && x[0] == k) return x
-      }
-    }
-  }
-
-  return null
-}
-
-var addIngredientsFunc = function() {
-  var jsonld = JSON.parse(document.querySelector('script[type="application/ld+json"]').innerText)
-  var title = document.title
-  var ingredients = findIngredientsFunc(jsonld, 'recipeIngredient')
-  var str = ingredients[1]
-  ////////////////////////////////////////
-  chrome.runtime.sendMessage({command: "AddAllIngredients", title: title, ingredients: str}, response => {
-    alert(response.status)
-  })
-}
